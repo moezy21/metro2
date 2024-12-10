f3999000f403a39820412fad09e7e71ad1db89c4
moezy21 <mohamad.al-razak@metropolia.fi>
2024-12-10 12:00:03 +0200

    Create tehtava1.js

---
+++ java3/tehtava1.js
@@ -0,0 +1,10 @@
+`use strict`
+
+
+
+const ul = document.getElementById("target");
+
+ul.classList.add(`my-list`)
+ul.innerHTML += `<li>first item</li>`
+ul.innerHTML += `<li>second item</li>`
+ul.innerHTML += `<li>third  item</li>`
